030ee7f8864d936385bc3511245c6261b986e5ef
jobayerimtiaz <jiefq.37@gmail.com>
2025-12-11 13:17:58 +0600

    Added protect admin middleware

--- routes/showRoutes.js
+++ routes/showRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addShow, getNowPlayingMovies } from "../controllers/showController.js";
+import { protectAdmin } from "../middleware/auth.js";
 
 const ShowRouter = express.Router();
 
-ShowRouter.get("/now-playing", getNowPlayingMovies);
+ShowRouter.get("/now-playing", protectAdmin, getNowPlayingMovies);
 
-ShowRouter.post("/add-show", addShow);
+ShowRouter.post("/add-show", protectAdmin, addShow);
 
 export default ShowRouter;
